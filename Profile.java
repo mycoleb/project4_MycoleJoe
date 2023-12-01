28f3e4116ae2d29403d450355ff8348fa0039065
Mycole Brown <mycole@uw.edu>
2023-12-01 15:29:31 -0500

    implemented profile and profile manager

---
+++ Profile.java
@@ -0,0 +1,69 @@
+//Profile.java
+import java.util.ArrayList;
+import java.util.List;
+public class Profile {
+    private String name;
+    private String image; // URL or path to the image; optional
+    private String status;
+    private List<Profile> friendProfiles;
+    // Additional attributes like location, age, etc., can be added here
+    public Profile(String name, String image, String status) {
+        this.name = name;
+        this.image = image;
+        this.status = status;
+        this.friendProfiles = new ArrayList<>();
+    }
+    // Getter and setter for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    // Getter and setter for image
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    // Getter and setter for status
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    // Method to add a friend
+    public void addFriend(Profile friendProfile) {
+        if (!friendProfiles.contains(friendProfile)) {
+            friendProfiles.add(friendProfile);
+        }
+    }
+    // Method to remove a friend
+    public void removeFriend(Profile friendProfile) {
+        friendProfiles.remove(friendProfile);
+    }
+    // Method to get the list of friends
+    public List<Profile> getFriends() {
+        return new ArrayList<>(friendProfiles);
+    }
+    // Method to print profile details
+    public void printProfileDetails() {
+        System.out.println("Name: " + name);
+        if (image != null && !image.isEmpty()) {
+            System.out.println("Image: " + image);
+        }
+        System.out.println("Status: " + status);
+        System.out.println("Friends:");
+        for (Profile friend : friendProfiles) {
+            System.out.println(friend.getName());
+        }
+    }
+}
+
+
+
+
+
+2:29
---
+++ ProfileManager.java
@@ -0,0 +1,72 @@
+//ProfileManager.java
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+public class ProfileManager {
+    private Map<String, Profile> profiles;
+    private UndirectedGraph<Profile> network;
+    public ProfileManager() {
+        profiles = new HashMap<>();
+        network = new UndirectedGraph<>();
+    }
+    // Method to add a profile to the network
+    public void addProfile(Profile profile) {
+        if (!profiles.containsKey(profile.getName())) {
+            profiles.put(profile.getName(), profile);
+            network.addVertex(profile);
+        }
+    }
+    // Method to remove a profile from the network
+    public void removeProfile(String profileName) {
+        if (profiles.containsKey(profileName)) {
+            Profile profile = profiles.get(profileName);
+            network.removeVertex(profile);
+            profiles.remove(profileName);
+        }
+    }
+    // Method to create a friendship between two profiles
+    public void createFriendship(String profileName1, String profileName2) {
+        if (profiles.containsKey(profileName1) && profiles.containsKey(profileName2)) {
+            Profile profile1 = profiles.get(profileName1);
+            Profile profile2 = profiles.get(profileName2);
+            network.addEdge(profile1, profile2);
+            profile1.addFriend(profile2);
+            profile2.addFriend(profile1);
+        }
+    }
+    // Method to display all profiles
+    public void displayAllProfiles() {
+        for (Profile profile : profiles.values()) {
+            profile.printProfileDetails();
+        }
+    }
+    // Method to display a profile's friends
+    public void displayFriends(String profileName) {
+        if (profiles.containsKey(profileName)) {
+            Profile profile = profiles.get(profileName);
+            System.out.println("Friends of " + profileName + ":");
+            for (Profile friend : profile.getFriends()) {
+                System.out.println(friend.getName());
+            }
+        }
+    }
+    // Method to display friends of friends
+    public void displayFriendsOfFriends(String profileName) {
+        if (profiles.containsKey(profileName)) {
+            Profile profile = profiles.get(profileName);
+            System.out.println("Friends of friends of " + profileName + ":");
+            Set<Profile> friendsOfFriends = network.getNeighbors(profile);
+            for (Profile fof : friendsOfFriends) {
+                if (!profile.getFriends().contains(fof)) {
+                    System.out.println(fof.getName());
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
